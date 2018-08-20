76c69c165468c50dee3c26995509c5544d54411a
Trang Tran <trang.tran@sharering.network>
2018-08-20 15:01:25 +0700

    REFACTOR: transaction into SHRTx interface. Prepare for AuthTx SPM-194

--- types/transaction.go
+++ types/transaction.go
@@ -5,48 +5,61 @@ import (
 	"fmt"
 
 	sdk "bitbucket.org/shareringvn/cosmos-sdk/types"
-    "bitbucket.org/shareringvn/cosmos-sdk/wire"
-
-
+	"bitbucket.org/shareringvn/cosmos-sdk/wire"
 )
+
+//-----------------------------------------------------------------
+// Tx Interface
+type SHRTx interface {
+	GetMsg() sdk.Msg
+	GetSignature() SHRSignature
+	VerifySignature() bool
+	GetSignBytes() []byte
+}
 
 //------------------------------------------------------------------
 // Tx
 
+var _ SHRTx = BasicTx{}
+
 // Simple tx to wrap the Msg.
-type SHRTx struct {
+type BasicTx struct {
 	sdk.Msg   `json:"message"`
-	Signature SHRSignature `json:"signature"`
+	Signature BaseSig `json:"signature"`
 }
 
-
-func NewSHRTx(msg sdk.Msg, sig SHRSignature) SHRTx {
-    return SHRTx{
-        Msg: msg,
-        Signature: sig,
-    }
+func NewBasicTx(msg sdk.Msg, sig BaseSig) BasicTx {
+	return BasicTx{
+		Msg:       msg,
+		Signature: sig,
+	}
 }
 
 // GetMsgs returns multiple messages
-func (tx SHRTx) GetMsgs() []sdk.Msg {
+func (tx BasicTx) GetMsgs() []sdk.Msg {
 	return []sdk.Msg{tx.Msg}
 }
 
 // GetMsg returns the message of this transaction
-func (tx SHRTx) GetMsg() sdk.Msg {
+func (tx BasicTx) GetMsg() sdk.Msg {
 	return tx.Msg
 }
 
 // GetSignature returns the signature with this transaction
-func (tx SHRTx) GetSignature() SHRSignature {
-	return tx.Signature
+func (tx BasicTx) GetSignature() SHRSignature {
+	return tx.Signature.(SHRSignature)
+}
+
+// GetSignBytes returns Bytes to be signed
+func (tx BasicTx) GetSignBytes() []byte {
+	return tx.Msg.GetSignBytes()
 }
 
 // VerifySignature to verify signature
-func (tx SHRTx) VerifySignature() bool {
-    msg := tx.Msg.GetSignBytes()
-    fmt.Printf("SignBytes: %s\n", msg)
-    return tx.Signature.Verify(msg)
+func (tx BasicTx) VerifySignature() bool {
+	msg := tx.GetSignBytes()
+	fmt.Printf("SignBytes: %s\n", msg)
+	return tx.Signature.Verify(msg)
 }
 
 // JSON decode MsgSend.
@@ -54,19 +67,19 @@ func GetTxDecoder(cdc *wire.Codec) func([]byte) (sdk.Tx, sdk.Error) {
 	return func(txBytes []byte) (sdk.Tx, sdk.Error) {
 		var tx = SHRTx{}
 
-        //fmt.Println("TxDecoder:", txBytes)
-        //err := json.Unmarshal(txBytes, &tx)
-        err := cdc.UnmarshalJSON(txBytes, &tx)
+		//fmt.Println("TxDecoder:", txBytes)
+		//err := json.Unmarshal(txBytes, &tx)
+		err := cdc.UnmarshalJSON(txBytes, &tx)
 
 		if err != nil {
 			fmt.Println("Error in decoding")
 			return nil, sdk.ErrTxDecode(err.Error())
 		}
 		fmt.Println("Decoded Tx:", tx)
-        isVerified := tx.VerifySignature()
-        if !isVerified {
-            return nil, sdk.ErrTxDecode("InvalidSignature")
-        }
+		isVerified := tx.VerifySignature()
+		if !isVerified {
+			return nil, sdk.ErrTxDecode("InvalidSignature")
+		}
 		return tx, nil
 	}
 }
@@ -74,23 +87,29 @@ func GetTxDecoder(cdc *wire.Codec) func([]byte) (sdk.Tx, sdk.Error) {
 //------------------------------------------------------------------
 // Signature
 
-type SHRSignature struct {
-	PubKey `json:"pub_key"`
+type SHRSignature interface {
+	String() string
+	Verify([]byte) bool
+}
+
+var _ SHRSignature = BaseSig{}
+
+type BaseSig struct {
+	PubKey    `json:"pub_key"`
 	Signature `json:"signature"`
 }
 
-func NewSHRSignature(key PubKey, sig Signature) SHRSignature {
-    return SHRSignature{
-        PubKey: key,
-        Signature: sig,
-    }
+func NewBaseSig(key PubKey, sig Signature) BaseSig {
+	return BaseSig{
+		PubKey:    key,
+		Signature: sig,
+	}
 }
 
-
-func (sig SHRSignature) String() string {
-    return fmt.Sprintf("SHRSignature{%s, %s}", sig.PubKey, sig.Signature)
+func (sig BaseSig) String() string {
+	return fmt.Sprintf("BaseSig{%s, %s}", sig.PubKey, sig.Signature)
 }
 
-func (sig SHRSignature) Verify(msg []byte) bool {
-    return sig.PubKey.VerifyBytes(msg, sig.Signature)
+func (sig BaseSig) Verify(msg []byte) bool {
+	return sig.PubKey.VerifyBytes(msg, sig.Signature)
 }
