02cfe5a7a8a6b5f1b80b2c6b38a6a01f4f2a1419
Trang Tran <trang.tran@sharering.network>
2018-08-28 17:08:00 +0700

    UPDATE: test.go and executable file

--- cmd/test.go
+++ cmd/test.go
@@ -4,8 +4,11 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	sdk "bitbucket.org/shareringvn/cosmos-sdk/types"
+	"github.com/btcsuite/btcd/btcec"
+	crypto "github.com/tendermint/go-crypto"
 
 	"github.com/sharering/shareledger/app"
 	"github.com/sharering/shareledger/types"
@@ -14,10 +17,14 @@ import (
 
 	"github.com/sharering/shareledger/x/asset"
 	amsg "github.com/sharering/shareledger/x/asset/messages"
+
 	//ahdl "github.com/sharering/shareledger/x/asset/handlers"
 
 	"github.com/sharering/shareledger/x/booking"
 	bmsg "github.com/sharering/shareledger/x/booking/messages"
+
+	sha3 "github.com/ethereum/go-ethereum/crypto/sha3"
+	"github.com/sharering/shareledger/x/auth"
 )
 
 func usingJson() {
@@ -74,7 +81,6 @@ func usingCodec() {
 	fmt.Println("*****")
 	msg1 := messages.MsgCheck{
 		Account: sdk.Address([]byte("123")),
-		Denom:   "SHR",
 	}
 
 	fmt.Println("Check Message:", msg1)
@@ -196,15 +202,206 @@ func printMsgComplete() {
 	fmt.Println("Marshalled:", res)
 	fmt.Printf("String format: %s\n", res)
 	fmt.Printf("ToString: %s\n", hex.EncodeToString(res))
+}
+
+func testMsgCreateSignedTx() {
+	pkBytes, err := hex.DecodeString("ab83994cf95abe45b9d8610524b3f8f8fd023d69f79449011cb5320d2ca180c5")
+
+	if err != nil {
+		fmt.Println("Error in DecodeString: ", err)
+		return
+	}
+
+	privKey_, pubKey_ := btcec.PrivKeyFromBytes(btcec.S256(), pkBytes)
+
+	serPubKey := pubKey_.SerializeUncompressed()
+	var pubKey types.PubKeySecp256k1
+	copy(pubKey[:], serPubKey[:65])
+	fmt.Printf("SerPubKey: %x\n", serPubKey)
+	fmt.Printf("PubKey: %x\n", serPubKey[:65])
+
+	address := pubKey.Address()
+	fmt.Printf("Address: %x\n", address)
+	uuid := "112233"
+	status := true
+	fee := int64(1)
+	hash := []byte("111111")
+
+	msgCreate := amsg.NewMsgCreate(
+		address,
+		hash,
+		uuid,
+		status,
+		fee,
+	)
+
+	signBytes := msgCreate.GetSignBytes()
+	fmt.Printf("SignBytes: %s\n", signBytes)
+	messageHash := crypto.Sha256(signBytes)
+	fmt.Printf("Hash: %x\n", messageHash)
+
+	signature, err := privKey_.Sign(messageHash)
+	if err != nil {
+		fmt.Println("Error in Sign:", err)
+		return
+	}
+	fmt.Printf("Raw Sig: %x %x\n", signature.R, signature.S)
+	serSig := signature.Serialize()
+	fmt.Printf("Signature: %x\n", serSig)
+
+	var ecSig types.SignatureSecp256k1
+	ecSig = append(ecSig, serSig...)
+
+	shrSig := types.NewBasicSig(pubKey, ecSig)
+	fmt.Println("Verify:", shrSig.Verify(signBytes))
+
+	tx := types.NewBasicTx(msgCreate, shrSig)
+	fmt.Println("Verify Signature:", tx.VerifySignature())
+
+	cdc := app.MakeCodec()
+	cdc = asset.RegisterCodec(cdc)
+	b, e := cdc.MarshalJSON(tx)
+	if e != nil {
+		fmt.Println("Error in encoding tx", e)
+	}
+	fmt.Printf("Tx encoded: %s\n", b)
+
+	bA, eA := cdc.MarshalJSON(serSig)
+	if eA != nil {
+		fmt.Println("Error:", eA)
+	}
+	fmt.Printf("SerializedSig: %s\n", bA)
+
+}
+
+func testMsgCreateNonceSignedTx() {
+	pkBytes, err := hex.DecodeString("ab83994cf95abe45b9d8610524b3f8f8fd023d69f79449011cb5320d2ca180c5")
+
+	if err != nil {
+		fmt.Println("Error in DecodeString: ", err)
+		return
+	}
+
+	privKey_, pubKey_ := btcec.PrivKeyFromBytes(btcec.S256(), pkBytes)
+
+	serPubKey := pubKey_.SerializeUncompressed()
+	var pubKey types.PubKeySecp256k1
+	copy(pubKey[:], serPubKey[:65])
+	fmt.Printf("SerPubKey: %x\n", serPubKey)
+	fmt.Printf("PubKey: %x\n", serPubKey[:65])
+
+	address := pubKey.Address()
+	fmt.Printf("Address: %x\n", address)
+	uuid := "112233"
+	status := true
+	fee := int64(1)
+	hash := []byte("111111")
+
+	msgCreate := amsg.NewMsgCreate(
+		address,
+		hash,
+		uuid,
+		status,
+		fee,
+	)
+
+	nonce := 1
+
+	signBytes := msgCreate.GetSignBytes()
+	fmt.Printf("SignBytes: %s\n", signBytes)
+
+	signBytesWithNonce := append([]byte(strconv.Itoa(nonce)), signBytes...)
+	fmt.Printf("SignBytesWithNonce: %s", signBytesWithNonce)
+
+	messageHash := crypto.Sha256(signBytesWithNonce)
+	fmt.Printf("Hash: %x\n", messageHash)
+
+	signature, err := privKey_.Sign(messageHash)
+	if err != nil {
+		fmt.Println("Error in Sign:", err)
+		return
+	}
+
+	fmt.Printf("Raw Sig: %x %x\n", signature.R, signature.S)
+	serSig := signature.Serialize()
+	fmt.Printf("Signature: %x\n", serSig)
+
+	var ecSig types.SignatureSecp256k1
+	ecSig = append(ecSig, serSig...)
+
+	shrSig := auth.NewAuthSig(pubKey, ecSig, int64(nonce))
+	fmt.Println("Verify:", shrSig.Verify(signBytes))
+
+	tx := auth.NewAuthTx(msgCreate, shrSig)
+	fmt.Println("Verify Signature:", tx.VerifySignature())
+
+	cdc := app.MakeCodec()
+	cdc = asset.RegisterCodec(cdc)
+	b, e := cdc.MarshalJSON(tx)
+	if e != nil {
+		fmt.Println("Error in encoding tx", e)
+	}
+	fmt.Printf("Tx encoded: %s\n", b)
+
+	bA, eA := cdc.MarshalJSON(serSig)
+	if eA != nil {
+		fmt.Println("Error:", eA)
+	}
+	fmt.Printf("SerializedSig: %s\n", bA)
+
+}
+
+func testPubKey() {
+	keyStr := "2b4d507ba002c4fa3d6394dbd911bb2ae49d2dbc70e0c80e368e967d409e87dbd8d95b0d50c0fe95410687e9679e334f41efb690853e061c14c00704f8901afc"
+	keyByte, err := hex.DecodeString(keyStr)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	var keySec types.PubKeySecp256k1
+	copy(keySec[:], keyByte[:])
+	fmt.Printf("Address: %x\n", keySec.Address())
+
+}
+
+func testKeccak() {
+	// str := []byte("2b4d507ba002c4fa3d6394dbd911bb2ae49d2dbc70e0c80e368e967d409e87dbd8d95b0d50c0fe95410687e9679e334f41efb690853e061c14c00704f8901afc")
+	str := "2b4d507ba002c4fa3d6394dbd911bb2ae49d2dbc70e0c80e368e967d409e87dbd8d95b0d50c0fe95410687e9679e334f41efb690853e061c14c00704f8901afc"
+	hash := sha3.NewKeccak256()
+
+	var buf []byte
+	keyByte := decodeHex(str)
+	var keySec types.PubKeySecp256k1
+	copy(keySec[:], keyByte[:])
+	hash.Write(keySec[:])
+	// hash.Write(str)
+	buf = hash.Sum(buf)
+	res := fmt.Sprintf("%x", buf)
+	fmt.Println(res)
+	fmt.Println("Length", len(res))
+}
+
+func decodeHex(s string) []byte {
+	b, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+
+	return b
+}
+
+func testSignature() {
 
 }
 
 func main() {
-	usingCodec()
-	//fmt.Println("******")
-	//usingJson()
+	//usingCodec()
 	//printMsgCreate()
 	//printMsgBook()
 	//printMsgLoad()
 	//printMsgComplete()
+	//testPubKey()
+	// testKeccak()
+	testMsgCreateSignedTx()
+	//testMsgCreateNonceSignedTx()
+
 }
