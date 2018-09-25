7dcb5b9f09a579f4aeadf3199dfc184272e6a88c
Trang Tran <trang.tran@sharering.network>
2018-09-25 15:22:24 +0700

    ADD: utilities for POS SPM-231

---
+++ types/stake.go
@@ -0,0 +1,85 @@
+package types
+
+import (
+	abci "github.com/tendermint/abci/types"
+	tmtypes "github.com/tendermint/tendermint/types"
+)
+
+// status of a validator
+type BondStatus byte
+
+// nolint
+const (
+	Unbonded  BondStatus = 0x00
+	Unbonding BondStatus = 0x01
+	Bonded    BondStatus = 0x02
+)
+
+//BondStatusToString for pretty prints of Bond Status
+func BondStatusToString(b BondStatus) string {
+	switch b {
+	case 0x00:
+		return "Unbonded"
+	case 0x01:
+		return "Unbonding"
+	case 0x02:
+		return "Bonded"
+	default:
+		return ""
+	}
+}
+
+// validator for a delegated proof of stake system
+type Validator interface {
+	GetMoniker() string       // moniker of the validator
+	GetStatus() BondStatus    // status of the validator
+	GetOwner() Address        // owner address to receive/return validators coins
+	GetPubKey() PubKey // validation pubkey
+	GetPower() Rat            // validation power
+	GetDelegatorShares() Rat  // Total out standing delegator shares
+	GetBondHeight() int64     // height in which the validator became active
+}
+
+// validator which fulfills abci validator interface for use in Tendermint
+func ABCIValidator(v Validator) abci.Validator {
+	return abci.Validator{
+		PubKey: tmtypes.TM2PB.PubKey(v.GetPubKey()),
+		Power:  v.GetPower().Evaluate(),
+	}
+}
+
+// properties for the set of all validators
+type ValidatorSet interface {
+	// iterate through validator by owner-address, execute func for each validator
+	IterateValidators(Context,
+		func(index int64, validator Validator) (stop bool))
+
+	// iterate through bonded validator by pubkey-address, execute func for each validator
+	IterateValidatorsBonded(Context,
+		func(index int64, validator Validator) (stop bool))
+
+	Validator(Context, Address) Validator     // get a particular validator by owner address
+	TotalPower(Context) Rat                   // total power of the validator set
+	Slash(Context, PubKey, int64, Rat) // slash the validator and delegators of the validator, specifying offence height & slash fraction
+	Revoke(Context, PubKey)            // revoke a validator
+	Unrevoke(Context, PubKey)          // unrevoke a validator
+}
+
+//_______________________________________________________________________________
+
+// delegation bond for a delegated proof of stake system
+type Delegation interface {
+	GetDelegator() Address // delegator address for the bond
+	GetValidator() Address // validator owner address for the bond
+	GetBondShares() Rat    // amount of validator's shares
+}
+
+// properties for the set of all delegations for a particular
+type DelegationSet interface {
+	GetValidatorSet() ValidatorSet // validator set for which delegation set is based upon
+
+	// iterate through all delegations from one delegator by validator-address,
+	//   execute func for each validator
+	IterateDelegations(ctx Context, delegator Address,
+		fn func(index int64, delegation Delegation) (stop bool))
+}
