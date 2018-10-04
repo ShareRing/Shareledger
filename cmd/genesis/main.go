7a2b15d09ab2418889bcab5d2eb02cd51ee9dd6f
Trang Tran <trang.tran@sharering.network>
2018-10-04 13:40:54 +0700

    UPDATE: convert pubkey in priv_validator.json from ed25519 to secp256k1 to match ShareLedger SPM-239

--- cmd/genesis/main.go
+++ cmd/genesis/main.go
@@ -12,8 +12,26 @@ import (
 	"github.com/sharering/shareledger/types"
 )
 
-func generateGenesisState(filePath string) app.GenesisState {
+const (
+	ConfigDir            string = "/.tendermint/config/"
+	PrivateValidatorFile string = "priv_validator.json"
+	GenesisFile          string = "genesis.json"
+)
+
+// Read priv_validator.jso
+// Replace ed25519 with secp256k1
+// return corresponding app.GenesisState
+func generateGenesisState(filePath string) (app.GenesisState, crypto.PubKey) {
 	pv := privval.LoadFilePV(filePath)
+
+	// Change Ed25519 to Secp256k1
+	newPrivKey := crypto.GenPrivKeySecp256k1()
+	pv.PrivKey = newPrivKey
+	pv.PubKey = pv.PrivKey.PubKey()
+	pv.Address = pv.PubKey.Address()
+
+	// save new priv_validator.json
+	pv.Save()
 
 	privK, ok := pv.PrivKey.(crypto.PrivKeySecp256k1)
 
@@ -21,18 +39,28 @@ func generateGenesisState(filePath string) app.GenesisState {
 		panic(ok)
 	}
 
+	// privKey in ShareLedger PrivKeySecp256k1
 	privKey := types.NewPrivKeySecp256k1(privK[:])
 	pubKey := privKey.PubKey()
 
 	gs := app.GenerateGenesisState(pubKey)
-	return gs
+	return gs, pv.PubKey
 }
 
-func generateGenesisFile(filePath string, genesisState []byte) {
+// Update GenesisFile with new Key
+// Update GenesisFile with AppState
+// Save new GenesisFile
+func generateGenesisFile(filePath string, genesisState []byte, pubKey crypto.PubKey) {
 	genesisDoc, err := tmtypes.GenesisDocFromFile(filePath)
 	if err != nil {
 		panic(err)
 	}
+
+	// Update genesisDoc with new validator
+	genesisDoc.Validators = []tmtypes.GenesisValidator{{
+		PubKey: pubKey,
+		Power:  10,
+	}}
 
 	genesisDoc.AppStateJSON = json.RawMessage(genesisState)
 
@@ -40,7 +68,10 @@ func generateGenesisFile(filePath string, genesisState []byte) {
 }
 
 func main() {
-	home := os.Getenv("HOME")
-	genesisState := generateGenesisState(home + "/.tendermint/config/priv_validator.json")
-	generateGenesisFile(home+"/.tendermint/config/genesis.json", genesisState.ToJSON())
+	homeDir := os.Getenv("HOME")
+	// reading priv_validator
+	genesisState, pubKey := generateGenesisState(homeDir + ConfigDir + PrivateValidatorFile)
+
+	// update genesis accordingly
+	generateGenesisFile(homeDir+ConfigDir+GenesisFile, genesisState.ToJSON(), pubKey)
 }
