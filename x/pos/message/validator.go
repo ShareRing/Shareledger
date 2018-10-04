20246b7649d88779baaa318d59c26c7520d52b40
TanDo <tan.do@sharering.network>
2018-10-04 16:22:30 +0700

    Merge remote-tracking branch 'origin/feature/pos_trang' into feature/pos
    # Conflicts:
    #	x/pos/genesis.go
    #	x/pos/params.go
    merge

---
+++ x/pos/message/validator.go
@@ -0,0 +1,79 @@
+package message
+
+import (
+	"bytes"
+	"encoding/json"
+
+	sdk "bitbucket.org/shareringvn/cosmos-sdk/types"
+	"github.com/sharering/shareledger/constants"
+	types "github.com/sharering/shareledger/types"
+	posTypes "github.com/sharering/shareledger/x/pos/type"
+)
+
+// MsgCreateValidator - struct for unbonding transactions
+type MsgCreateValidator struct {
+	Description   posTypes.Description
+	DelegatorAddr sdk.Address  `json:"delegator_address"`
+	ValidatorAddr sdk.Address  `json:"validator_address"`
+	PubKey        types.PubKey `json:"pubkey"`
+	Delegation    types.Coin   `json:"delegation"`
+}
+
+// MsgSend implements sdk.Msg
+var _ sdk.Msg = MsgCreateValidator{}
+
+// Type Implements Msg
+func (msg MsgCreateValidator) Type() string {
+	return constants.MESSAGE_POS
+}
+
+// Return address(es) that must sign over msg.GetSignBytes()
+func (msg MsgCreateValidator) GetSigners() []sdk.Address {
+	// delegator is first signer so delegator pays fees
+	addrs := []sdk.Address{msg.DelegatorAddr}
+
+	if !bytes.Equal(msg.DelegatorAddr.Bytes(), msg.ValidatorAddr.Bytes()) {
+		// if validator addr is not same as delegator addr, validator must sign
+		// msg as well
+		addrs = append(addrs, sdk.Address(msg.ValidatorAddr))
+	}
+	return addrs
+}
+
+// get the bytes for the message signer to sign on
+func (msg MsgCreateValidator) GetSignBytes() []byte {
+	b, err := json.Marshal(struct {
+		Description   posTypes.Description
+		DelegatorAddr sdk.Address  `json:"delegator_address"`
+		ValidatorAddr sdk.Address  `json:"validator_address"`
+		PubKey        types.PubKey `json:"pubkey"`
+		Delegation    types.Coin   `json:"delegation"`
+	}{
+		Description:   msg.Description,
+		ValidatorAddr: msg.ValidatorAddr,
+		PubKey:        msg.PubKey,
+		Delegation:    msg.Delegation,
+	})
+	if err != nil {
+		panic(err)
+	}
+	return b //sdk.MustSortJSON(b)
+}
+
+// quick validity check
+func (msg MsgCreateValidator) ValidateBasic() sdk.Error {
+	if msg.DelegatorAddr == nil {
+		return posTypes.ErrNilDelegatorAddr(posTypes.DefaultCodespace)
+	}
+	if msg.ValidatorAddr == nil {
+		return posTypes.ErrNilValidatorAddr(posTypes.DefaultCodespace)
+	}
+	if !(msg.Delegation.IsPositive()) {
+		return posTypes.ErrBadDelegationAmount(posTypes.DefaultCodespace)
+	}
+	if msg.Description == (posTypes.Description{}) {
+		return sdk.NewError(posTypes.DefaultCodespace, posTypes.CodeInvalidInput, "description must be included")
+	}
+
+	return nil
+}
