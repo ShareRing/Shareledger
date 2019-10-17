c2ba786ee711c2e280960610b1029a6738ee1d44
phuoc <phuocdongxoai@gmail.com>
2019-10-17 13:51:25 +0700

    [FIX] Router not in baseapp

--- cosmos-wrapper/baseapp/router.go
+++ cosmos-wrapper/baseapp/router.go
@@ -2,12 +2,11 @@ package baseapp
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	bapp "github.com/cosmos/cosmos-sdk/baseapp"
 
 	types "github.com/sharering/shareledger/cosmos-wrapper/types"
 )
 
-func AddRoute(app *BaseApp, path string, h types.Handler) bapp.Router {
+func AddRoute(app *BaseApp, path string, h types.Handler) sdk.Router {
 
 	// Wrap around every handler to ensure Fee is Called
 	newHandler := func(ctx sdk.Context, msg sdk.Msg) sdk.Result {
