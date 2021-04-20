f0f2b4586bd4341f99155a63392712aacf8846f2
phu <phu.khong@sharering.network>
2021-04-20 15:22:41 +0700

    Test fee document module

--- tests/gentlemint_load_and_burn_test.go
+++ tests/gentlemint_load_and_burn_test.go
@@ -27,7 +27,7 @@ func TestGentlemint_LoadSHR_Success(t *testing.T) {
 	loadCmd := fmt.Sprintf("load-shr %s %s", treasurer.String(), amount)
 
 	// Load
-	_, stdOut, _ := f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes --fees 1shr", authority.String()))
+	_, stdOut, _ := f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes", authority.String()))
 	// wait for a block confirmation
 	tests.WaitForNextNBlocksTM(1, f.Port)
 
@@ -57,11 +57,11 @@ func TestGentlemint_BurnSHR_Success(t *testing.T) {
 	loadCmd := fmt.Sprintf("load-shr %s %s", treasurer.String(), amount)
 
 	// Load
-	f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes --fees 1shr", authority.String()))
+	f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes", authority.String()))
 	// wait for a block confirmation
 	tests.WaitForNextNBlocksTM(1, f.Port)
 
-	_, stdOut, _ := f.ExecuteGentlemintTxCommand(burnCmd, fmt.Sprintf("--from %s --yes --fees 1shr", treasurer.String()))
+	_, stdOut, _ := f.ExecuteGentlemintTxCommand(burnCmd, fmt.Sprintf("--from %s --yes", treasurer.String()))
 
 	txRepsonse := ParseStdOut(t, stdOut)
 	require.Equal(t, uint32(0), txRepsonse.Code)
@@ -88,7 +88,7 @@ func TestGentlemint_Enroll_ShrpLoader_Success(t *testing.T) {
 	enrollLoaderCmd := fmt.Sprintf("enroll-loaders %s", treasurer.String())
 
 	// Load
-	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes --fees 1shr", authority.String()))
+	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes", authority.String()))
 	// wait for a block confirmation
 	tests.WaitForNextNBlocksTM(1, f.Port)
 
@@ -118,14 +118,14 @@ func TestGentlemint_Revoke_ShrpLoader_Success(t *testing.T) {
 	revokeLoaderCmd := fmt.Sprintf("revoke-loaders %s", treasurer.String())
 
 	// Enroll
-	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes --fees 1shr", authority.String()))
+	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes", authority.String()))
 	tests.WaitForNextNBlocksTM(1, f.Port)
 
 	txRepsonse := ParseStdOut(t, stdOut)
 	require.Equal(t, uint32(0), txRepsonse.Code)
 
 	// Revoke
-	_, stdOut2, _ := f.ExecuteGentlemintTxCommand(revokeLoaderCmd, fmt.Sprintf("--from %s --yes --fees 1shr", authority.String()))
+	_, stdOut2, _ := f.ExecuteGentlemintTxCommand(revokeLoaderCmd, fmt.Sprintf("--from %s --yes", authority.String()))
 	tests.WaitForNextNBlocksTM(1, f.Port)
 
 	txRepsonse2 := ParseStdOut(t, stdOut2)
@@ -157,14 +157,14 @@ func TestGentlemint_LoadSHRP_Success(t *testing.T) {
 	enrollLoaderCmd := fmt.Sprintf("enroll-loaders %s", treasurer.String())
 
 	// Enroll
-	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes --fees 1shr", authority.String()))
+	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes", authority.String()))
 	tests.WaitForNextNBlocksTM(1, f.Port)
 
 	txRepsonse := ParseStdOut(t, stdOut)
 	require.Equal(t, uint32(0), txRepsonse.Code)
 
 	// Load
-	_, stdOut2, _ := f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes --fees 1shr", treasurer.String()))
+	_, stdOut2, _ := f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes", treasurer.String()))
 
 	txRepsonse2 := ParseStdOut(t, stdOut2)
 	require.Equal(t, uint32(0), txRepsonse2.Code)
@@ -202,19 +202,19 @@ func TestGentlemint_BurnSHRP_Success(t *testing.T) {
 	burnCmd := fmt.Sprintf("burn-shrp %s", burnAmount)
 
 	// Enroll
-	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes --fees 1shr", authority.String()))
+	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes", authority.String()))
 
 	txRepsonse := ParseStdOut(t, stdOut)
 	require.Equal(t, uint32(0), txRepsonse.Code)
 
 	// Load
-	_, stdOut2, _ := f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes --fees 1shr", treasurer.String()))
+	_, stdOut2, _ := f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes", treasurer.String()))
 
 	txRepsonse2 := ParseStdOut(t, stdOut2)
 	require.Equal(t, uint32(0), txRepsonse2.Code)
 
 	// Burn
-	_, stdOut3, _ := f.ExecuteGentlemintTxCommand(burnCmd, fmt.Sprintf("--from %s --yes --fees 1shr", treasurer.String()))
+	_, stdOut3, _ := f.ExecuteGentlemintTxCommand(burnCmd, fmt.Sprintf("--from %s --yes", treasurer.String()))
 
 	txRepsonse3 := ParseStdOut(t, stdOut3)
 	require.Equal(t, uint32(0), txRepsonse3.Code)
@@ -238,7 +238,7 @@ func TestGentlemint_SetExchangeRate_Success(t *testing.T) {
 	rate := "99"
 	setExchageRateCmd := fmt.Sprintf("set-exchange %s", rate)
 
-	_, stdOut, _ := f.ExecuteGentlemintTxCommand(setExchageRateCmd, fmt.Sprintf("--from %s --yes --fees 1shr", treasurer.String()))
+	_, stdOut, _ := f.ExecuteGentlemintTxCommand(setExchageRateCmd, fmt.Sprintf("--from %s --yes", treasurer.String()))
 	tests.WaitForNextNBlocksTM(1, f.Port)
 
 	txRepsonse := ParseStdOut(t, stdOut)
@@ -275,19 +275,19 @@ func TestGentlemint_BuySHR_Success(t *testing.T) {
 	buyCmd := fmt.Sprintf("buy-shr %s", buyAmount)
 
 	// Enroll
-	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes --fees 1shr", authority.String()))
+	_, stdOut, _ := f.ExecuteGentlemintTxCommand(enrollLoaderCmd, fmt.Sprintf("--from %s --yes", authority.String()))
 
 	txRepsonse := ParseStdOut(t, stdOut)
 	require.Equal(t, uint32(0), txRepsonse.Code)
 
 	// Load
-	_, stdOut2, _ := f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes --fees 1shr", treasurer.String()))
+	_, stdOut2, _ := f.ExecuteGentlemintTxCommand(loadCmd, fmt.Sprintf("--from %s --yes", treasurer.String()))
 
 	txRepsonse2 := ParseStdOut(t, stdOut2)
 	require.Equal(t, uint32(0), txRepsonse2.Code)
 
 	// Buy
-	_, stdOut3, _ := f.ExecuteGentlemintTxCommand(buyCmd, fmt.Sprintf("--from %s --yes --fees 1shr", treasurer.String()))
+	_, stdOut3, _ := f.ExecuteGentlemintTxCommand(buyCmd, fmt.Sprintf("--from %s --yes", treasurer.String()))
 
 	txRepsonse3 := ParseStdOut(t, stdOut3)
 	require.Equal(t, uint32(0), txRepsonse3.Code)
