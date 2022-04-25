aa0a13e453be0cb8e5b9a969884872858e829535
Khang Pham <khang.p@sharering.network>
2022-04-25 16:00:47 +0700

    refund shr when user cancel the swapping request

--- x/swap/keeper/keeper_cancel_swap.go
+++ x/swap/keeper/keeper_cancel_swap.go
@@ -4,6 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/sharering/shareledger/x/swap/types"
+	denom "github.com/sharering/shareledger/x/utils/demo"
 )
 
 func (k Keeper) CancelSwap(ctx sdk.Context, msg *types.MsgCancel) error {
@@ -22,8 +23,32 @@ func (k Keeper) CancelSwap(ctx sdk.Context, msg *types.MsgCancel) error {
 			return sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "your address %s isn't owner of swap request id=%s", txCreator, reqSrcAddr)
 		}
 	}
+
+	refunds := make(map[string]sdk.DecCoins)
 	for i := range requests {
-		pendingStore.Delete(GetRequestIDBytes(requests[i].GetId()))
+		rq := requests[i]
+		pendingStore.Delete(GetRequestIDBytes(rq.GetId()))
+		total, found := refunds[rq.GetSrcAddr()]
+		if !found {
+			total = sdk.NewDecCoins()
+		}
+		total = total.Add(*rq.GetAmount()).Add(*rq.GetFee())
+		refunds[rq.GetSrcAddr()] = total
 	}
+
+	for addr, coin := range refunds {
+		add, err := sdk.AccAddressFromBech32(addr)
+		if err != nil {
+			return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "%+v", err)
+		}
+		bc, err := denom.NormalizeToBaseCoins(coin, false)
+		if err != nil {
+			return sdkerrors.Wrapf(sdkerrors.ErrLogic, "%v", err)
+		}
+		if err = k.bankKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleName, add, bc); err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
