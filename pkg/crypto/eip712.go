1e99379418e8df8eb59b5aa5cce62eac148c111d
hoai nguyen <hoai.n@sharering.network>
2022-04-27 14:37:42 +0700

    add eip712 encode

---
+++ pkg/crypto/eip712.go
@@ -0,0 +1,28 @@
+package crypto
+
+import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/signer/core/apitypes"
+)
+
+func Keccak256HashEIP712(signData apitypes.TypedData) (common.Hash, error) {
+	bSignData, err := encodeForEIP712Signing(signData)
+	if err != nil {
+		return common.Hash{}, err
+	}
+	return crypto.Keccak256Hash(bSignData), nil
+}
+
+func encodeForEIP712Signing(signData apitypes.TypedData) ([]byte, error) {
+	domainSeparator, err := signData.HashStruct("EIP712Domain", signData.Domain.Map())
+	if err != nil {
+		return nil, err
+	}
+	typedDataHash, err := signData.HashStruct(signData.PrimaryType, signData.Message)
+	if err != nil {
+		return nil, err
+	}
+	return []byte(fmt.Sprintf("\x19\x01%s%s", string(domainSeparator), string(typedDataHash))), nil
+}
