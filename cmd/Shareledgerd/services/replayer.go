5785a6248af47ca227c8f75373b522ab75263411
Khang Pham <khang.p@sharering.network>
2022-05-10 09:56:49 +0700

    adding scaffold for make the swap in and out request

--- cmd/Shareledgerd/services/replayer.go
+++ cmd/Shareledgerd/services/replayer.go
@@ -7,9 +7,11 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	"github.com/cosmos/cosmos-sdk/server"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/pkg/errors"
 	swapmoduletypes "github.com/sharering/shareledger/x/swap/types"
+	denom "github.com/sharering/shareledger/x/utils/demo"
 	"github.com/spf13/cobra"
 	"strings"
 	"time"
@@ -64,7 +66,7 @@ func NewStartCommands(defaultNodeHome string) *cobra.Command {
 
 			ctx, cancel := context.WithCancel(context.Background())
 			go func() {
-				server.WaitForQuitSignals()
+				_ = server.WaitForQuitSignals()
 				cancel()
 			}()
 			relayerClient := initRelayer(clientTx, mapNetworkSigners, "https://eth-ropsten.alchemyapi.io/v2/0M8yP6-iyIof8dFJN0Jph59jJlSKqmbW")
@@ -125,13 +127,22 @@ type relayer struct {
 }
 
 func (r *relayer) startOutProcess(ctx context.Context) error {
-	// TODO: Get pending batches
-	//var batches []swapmoduletypes.Batch
-	// batches := getPendingBatches()
-	// processingBatch(batches[0].id)
-	// submitBatch(...)
-	// TODO: Change batch to pending
-	// TODO: fw the current batch to sc
+
+	var batches []swapmoduletypes.Batch
+	batches = r.getPendingBatches()
+
+	for i := range batches {
+		bRes, err := r.processingBatch(batches[i].Id)
+		if err != nil {
+			return err
+		}
+		//TODO handle the txsHash from ETH later after change the batch's structure
+		_, err = r.submitBatch(ctx, bRes)
+		if err != nil {
+			return err
+		}
+
+	}
 
 	return nil
 }
@@ -151,32 +162,43 @@ func (r *relayer) getPendingBatches() []swapmoduletypes.Batch {
 
 }
 
-func (r *relayer) processingBatch(batchId uint64) (swapmoduletypes.Batch, error) {
+func (r *relayer) updateBatch(msg *swapmoduletypes.MsgUpdateBatch) (swapmoduletypes.Batch, error) {
 	mClient := swapmoduletypes.NewMsgClient(r.Client)
 	qClient := swapmoduletypes.NewQueryClient(r.Client)
-	updateMsg := &swapmoduletypes.MsgUpdateBatch{
-		Creator: r.Client.GetFromAddress().String(),
-		BatchId: batchId,
-		Status:  swapmoduletypes.BatchStatusProcessing,
-	}
-	_, err := mClient.UpdateBatch(context.Background(), updateMsg)
-	if err != nil {
-		return swapmoduletypes.Batch{}, errors.Wrapf(err, "update batch id %d to processing fail", batchId)
-	}
 
+	_, err := mClient.UpdateBatch(context.Background(), msg)
+	if err != nil {
+		return swapmoduletypes.Batch{}, errors.Wrapf(err, "update batch id %d to processing fail", msg.GetBatchId())
+	}
 	batchIdReq := &swapmoduletypes.QueryBatchesRequest{
-		Ids: []uint64{batchId},
+		Ids: []uint64{msg.GetBatchId()},
 	}
 
 	batchesRes, err := qClient.Batches(context.Background(), batchIdReq)
 
 	if err != nil {
-		return swapmoduletypes.Batch{}, errors.Wrapf(err, "geting batch id %d fail", batchId)
+		return swapmoduletypes.Batch{}, errors.Wrapf(err, "geting batch id %d fail", msg.GetBatchId())
 	}
 	if len(batchesRes.GetBatches()) != 0 {
-		return swapmoduletypes.Batch{}, fmt.Errorf("batches reponse is empty")
+		return swapmoduletypes.Batch{}, fmt.Errorf("batches response is empty")
 	}
 	return batchesRes.GetBatches()[0], nil
+}
+
+func (r *relayer) processingBatch(batchId uint64) (swapmoduletypes.Batch, error) {
+
+	updateMsg := &swapmoduletypes.MsgUpdateBatch{
+		Creator: r.Client.GetFromAddress().String(),
+		BatchId: batchId,
+		Status:  swapmoduletypes.BatchStatusProcessing,
+	}
+
+	batch, err := r.updateBatch(updateMsg)
+	if err != nil {
+		return swapmoduletypes.Batch{}, nil
+	}
+
+	return batch, nil
 }
 
 func (r *relayer) submitBatch(ctx context.Context, batches swapmoduletypes.Batch) (txHash string, err error) {
@@ -225,6 +247,82 @@ func (r *relayer) submitBatch(ctx context.Context, batches swapmoduletypes.Batch
 	panic("implement me")
 }
 
+//TODO background or goroutine handle here
+func (r *relayer) listenETHSwap(ctx context.Context) (swapData struct{}, err error) {
+	//conn, err := ethclient.Dial(r.EthUrl)
+	//if err != nil {
+	//	return "", sdkerrors.Wrapf(sdkerrors.ErrLogic, err.Error())
+	//}
+	//swapClient, err := swap.NewSwap(common.HexToAddress("0xC5eAdD9b5ea60A991a65888ECC8F26FbDdc7Dbf4"), conn)
+	//if err != nil {
+	//	return "", sdkerrors.Wrapf(sdkerrors.ErrLogic, err.Error())
+	//}
+	//info, err := r.Client.Keyring.Key(r.Signers["erc20"])
+	//if err != nil {
+	//	return "", err
+	//}
+	//var priv types.PrivKey
+	//
+	//switch i := info.(type) {
+	//case info:
+	//	if i.PrivKeyArmor == "" {
+	//		return "nil, nil", fmt.Errorf("private key not available")
+	//	}
+	//	priv, err = legacy.PrivKeyFromBytes([]byte(i.PrivKeyArmor))
+	//	if err != nil {
+	//		return nil, nil, err
+	//	}
+	//default:
+	//	return nil, nil, fmt.Errorf("eip712 currently supports for local key")
+	//}
+	//secp256k1Priv, ok := priv.(*secp256k1.PrivKey)
+	//
+	//bind.NewKeyedTransactorWithChainID()
+	//
+	//swapClient.Swap(bind.TransactOpts{
+	//	From:      common.Address{},
+	//	Nonce:     nil,
+	//	Signer:    nil,
+	//	Value:     nil,
+	//	GasPrice:  nil,
+	//	GasFeeCap: nil,
+	//	GasTipCap: nil,
+	//	GasLimit:  0,
+	//	Context:   nil,
+	//	NoSend:    false,
+	//})
+	panic("implement me")
+}
+
 func (r *relayer) startInProcess(ctx context.Context) error {
+	//TODO concurrency handle here
+	_, err := r.listenETHSwap(ctx)
+	if err != nil {
+		return err
+	}
+	//Mocking the input
+	return r.initSwapInRequest(ctx, "0xxa", "shareledger1w4l5fchs69d9avlgvdehq9ypvdh4xyev3p490g", "erc20", 100, 15)
+}
+
+func (r *relayer) initSwapInRequest(
+	ctx context.Context,
+	destAddr, slp3Addr, srcNet string,
+	amount, fee uint64) error {
+	swapAmount := sdk.NewDecCoin(denom.Shr, sdk.NewIntFromUint64(amount))
+	swapFee := sdk.NewDecCoin(denom.Shr, sdk.NewIntFromUint64(fee))
+
+	msgClient := swapmoduletypes.NewMsgClient(r.Client)
+	inMsg := swapmoduletypes.NewMsgRequestIn(
+		r.Client.GetFromAddress().String(),
+		slp3Addr,
+		destAddr,
+		srcNet,
+		swapAmount,
+		swapFee,
+	)
+	_, err := msgClient.RequestIn(ctx, inMsg)
+	if err != nil {
+		return err
+	}
 	return nil
 }
