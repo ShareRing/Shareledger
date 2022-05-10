07904e0aebcc0824b130fd5de90e192428a7addf
hoai nguyen <hoai.n@sharering.network>
2022-05-10 11:52:13 +0700

    update some params

--- x/swap/keeper/msg_server_update_batch.go
+++ x/swap/keeper/msg_server_update_batch.go
@@ -30,9 +30,6 @@ func (k msgServer) UpdateBatch(goCtx context.Context, msg *types.MsgUpdateBatch)
 	if batch.GetStatus() != requireStatus {
 		return nil, sdkerrors.Wrapf(sdkerrors.ErrInvalidRequest, "updating batch status [%s] => [%s] is not allowed", batch.GetStatus(), msg.GetStatus())
 	}
-	if msg.GetNonce() != 0 {
-		batch.Nonce = msg.GetNonce()
-	}
 
 	batch.Status = msg.GetStatus()
 	k.SetBatch(ctx, batch)
